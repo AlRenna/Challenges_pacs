33fbc2acedb75f854072f2fecd3e267b649d0ea4
marzomatti <mattia.marzotto@mail.polimi.it>
2023-04-28 16:31:23 +0000

    always generates a null pointer, but it compiles

--- Challenge_new_2/SolverFactory.hpp
+++ Challenge_new_2/SolverFactory.hpp
@@ -2,83 +2,37 @@
 #define SOLVERFACTORY_HPP
 
 #include "Solver.hpp"
-#include<iostream>
 #include <memory>
+#include <tuple>
+#include <utility>
 
-
-enum class SolverType
-{
+enum class SolverType {
   Newton_solver,
   QuasiNewton_solver,
   Bisection_solver,
-  Secant_solver,
-  Base_solver
+  Secant_solver
 };
 
-inline
-std::unique_ptr<BaseSolver>
-make_solver_simple(SolverType s)
-{
-  switch(s)
-    {
-    case SolverType::Newton_solver:{
-        std::cout<<"Made pointer to newtonsolver!!!"<<std::endl;
-      return std::make_unique<NewtonSolver>();}
-    case SolverType::QuasiNewton_solver:
-      return std::make_unique<QuasiNewtonSolver>();
-    case SolverType::Bisection_solver:
-      return std::make_unique<BisectionSolver>();
-    case SolverType::Secant_solver:
-      return std::make_unique<SecantSolver>();
-    default:
-      return nullptr;
+template <typename T, typename... Args>
+std::unique_ptr<T> make_solver_helper(SolverType solver_type, std::tuple<Args...>&& args) {
+  if constexpr (std::is_constructible_v<T, Args...>) {
+    if (solver_type == SolverType::Newton_solver) {
+      return std::make_unique<NewtonSolver>(std::forward<Args>(std::get<Args>(args))...);
+    } else if (solver_type == SolverType::QuasiNewton_solver) {
+      return std::make_unique<QuasiNewtonSolver>(std::forward<Args>(std::get<Args>(args))...);
+    } else if (solver_type == SolverType::Bisection_solver) {
+      return std::make_unique<BisectionSolver>(std::forward<Args>(std::get<Args>(args))...);
+    } else if (solver_type == SolverType::Secant_solver) {
+      return std::make_unique<SecantSolver>(std::forward<Args>(std::get<Args>(args))...);
     }
-} 
-
-
-std::unique_ptr<BaseSolver> make_Newton_solver(
-        const T::FunctionType &f_, 
-        const T::VariableType &x0_, 
-        const T::VariableType &xf_, 
-        const unsigned int &max_it, 
-        const T::FunctionType &df_,
-        const T::VariableType &h_)
-                {return std::make_unique<NewtonSolver>(f_,x0_,xf_,max_it,df_,h_);}
-
-std::unique_ptr<BaseSolver> make_QuasiNewton_solver(
-        const T::FunctionType &f_, 
-        const T::VariableType &x0_, 
-        const T::VariableType &xf_, 
-        const unsigned int &max_it, 
-        const T::VariableType &h_)
-                {return std::make_unique<QuasiNewtonSolver>(f_,x0_,xf_,max_it,h_);}
-
-/* std::unique_ptr<BaseSolver> make_Bisection_solver(double tol, int max_iter)
-{
-  return std::make_unique<BisectionSolver>(tol, max_iter);
+  }
+  return nullptr;
 }
 
-std::unique_ptr<BaseSolver> make_Secant_solver(double tol, int max_iter)
-{
-  return std::make_unique<SecantSolver>(tol, max_iter);
-} */
-
-template <class... Args>
-std::unique_ptr<BaseSolver> make_solver(SolverType s, Args &&... args)
-{
-  switch(s)
-    {
-    case SolverType::Newton_solver:
-      return make_Newton_solver(std::forward<Args>(args)...);
-    case SolverType::QuasiNewton_solver:
-      return make_QuasiNewton_solver(std::forward<Args>(args)...);
-    /* case SolverType::Bisection_solver:
-      return make_Bisection_solver(std::forward<Args>(args)...);
-    case SolverType::Secant_solver:
-      return make_Secant_solver(std::forward<Args>(args)...); */
-    default:
-      return nullptr;
-    }
+template <typename... Args>
+std::unique_ptr<BaseSolver> make_solver(SolverType type, Args&&... args) {
+  auto t = std::make_tuple(std::forward<Args>(args)...);
+  return make_solver_helper<BaseSolver>(type, std::move(t));
 }
 
 #endif /* SOLVERFACTORY_HPP */
--- Challenge_new_2/main.cpp
+++ Challenge_new_2/main.cpp
@@ -1,43 +1,32 @@
 #include "Solver.hpp"
 #include "SolverFactory.hpp"
-
 #include <iostream>
 #include <cmath>
 #include <fstream>
 #include <iomanip>
+#include <tuple>
 
-int main(){
+int main() {
+    SolverTraits::FunctionType f = [&](double x) { return -x * exp(-x) - 2; };
 
-    T::FunctionType f = [&](double x){
-        return -x*exp(-x) -2;    
-    };
-    
-    T::FunctionType df = [&](double x){
-        return -exp(-x) + x*exp(-x) ;
-    };
-    
-    T::VariableType x0 = -1, xf = 2;
+    SolverTraits::FunctionType df = [&](double x) { return -exp(-x) + x * exp(-x); };
+
+    SolverTraits::VariableType x0 = -1, xf = 2;
     const unsigned int max_it = 100;
-    T::VariableType h=0.02;
+    SolverTraits::VariableType h = 0.02;
+
+    auto Nsolver = make_solver(SolverType::Newton_solver, f, x0, xf, max_it, df, h);
+    if (Nsolver)
+        Nsolver->solve();
+    else
+        std::cout<< "NULL POINTER!!!"<<std::endl;
 
 
-    /// Derived Solvers
-    BaseSolver Bsolver;
-
-
-    //auto NNsolver = make_solver_simple(SolverType::Newton_solver);
-
-    auto Nsolver = make_solver(SolverType::Newton_solver,f,x0,xf,max_it,df,h);
-    Nsolver->solve();
-
-    auto QNsolver = make_solver(SolverType::QuasiNewton_solver,f,x0,xf,max_it,h);
-    QNsolver->solve();
-
-
-   /*  auto BiSolver = make_solver<BisectionSolver,SolverType::BisectionSolver>(f,x0,xf,max_iter);
-    BiSolver->solve(); */
-
-    // std::cout<< result << std::endl;
+    auto QNsolver = make_solver(SolverType::QuasiNewton_solver, f, x0, xf, max_it, h);
+    if (QNsolver)
+        QNsolver->solve();
+    else
+        std::cout<< "NULL POINTER!!!"<<std::endl;
 
     return 0;
 }
